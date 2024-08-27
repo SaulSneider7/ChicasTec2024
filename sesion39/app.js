9d0574d304a1448aa614652634fba0d505e77aea
estudiante-juanito <jpariona@crackthecode.la>
2024-08-27 09:47:51 -0500

    pop

--- sesion39/app.js
+++ sesion39/app.js
@@ -1,6 +1,22 @@
-let frutas = ["Manzana", "Mandarina"];
+let frutas = ["Manzana", "Mandarina", "Piña"];
+frutas.pop();
 console.log(frutas);
 
-frutas.push("Fresa");
-console.log(frutas);
+// frutas.push("Fresa");
+// console.log(frutas);
+
+function conFe() {
+    //Recuperar el valor el input
+    let input = document.getElementById("texto").value;
+    //ejecutar el metodo push
+    frutas.push(input);
+    //mostrarlo en consola
+    console.log(frutas);
+    //Rezar para que salga bien
+}
+
+function eliminar() {
+    //ejecutar el metodo pop
+    //muestras por consola
+}
 
